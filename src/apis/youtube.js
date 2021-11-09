04f07dce7993f203666c151f8b3a252d5d0fc279
Jason Toups <me@jasontoups.com>
2021-11-08 17:14:50 -0800

    initial scaffolding

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = '' '';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		type: 'video',
+		maxResults: 5,
+		key: `${KEY}`,
+	},
+});
