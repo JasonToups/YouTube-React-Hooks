d1ebbacf9d07acf7f19293c827fa1627f3e79242
Jason Toups <me@jasontoups.com>
2021-11-11 11:15:24 -0800

    Updates

---
+++ precache-manifest.a4a3afa98b508bff2708f29bc7470b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7e46936a5c8f3bc7deebde5bce434d",
+    "url": "/YouTube-React-Hooks/index.html"
+  },
+  {
+    "revision": "6dbb93b46b2541fcc49e",
+    "url": "/YouTube-React-Hooks/static/css/main.21c6e085.chunk.css"
+  },
+  {
+    "revision": "78e96be91352d04a3ef1",
+    "url": "/YouTube-React-Hooks/static/js/2.83f62f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YouTube-React-Hooks/static/js/2.83f62f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbb93b46b2541fcc49e",
+    "url": "/YouTube-React-Hooks/static/js/main.7d6a3ac4.chunk.js"
+  },
+  {
+    "revision": "54b833ed97506e1189af",
+    "url": "/YouTube-React-Hooks/static/js/runtime-main.597a8311.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YouTube-React-Hooks/static/media/reactLogo.5d5d9eef.svg"
+  }
+]);
